842a335e7bbc9d09088b7d3ebfa478713ca7c716
Michał Witczak <michal.witczak@fi.abb.com>
2019-12-03 23:25:55 +0100

    Updates

---
+++ precache-manifest.e495233ad2a29ff879fa86012a353c36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fefe660e65d4bf9f1d55514a6f119c9",
+    "url": "/bieg-generala/index.html"
+  },
+  {
+    "revision": "1c7a7dd26fe634bd0340",
+    "url": "/bieg-generala/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "bc8a8edfd1384a17e987",
+    "url": "/bieg-generala/static/js/2.96cf753a.chunk.js"
+  },
+  {
+    "revision": "1c7a7dd26fe634bd0340",
+    "url": "/bieg-generala/static/js/main.08dcc21d.chunk.js"
+  },
+  {
+    "revision": "5b820b84a147f46fb760",
+    "url": "/bieg-generala/static/js/runtime-main.b998417d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/bieg-generala/static/media/logo.25bf045c.svg"
+  }
+]);
