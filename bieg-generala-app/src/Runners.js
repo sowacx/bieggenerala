6143846c064e7dd626a51c868a1d5bbe2f141676
Michał Witczak <mich.witczak@gmail.com>
2019-12-09 23:35:25 +0100

    working simple tables, next step use react-table

--- bieg-generala-app/src/Runners.js
+++ bieg-generala-app/src/Runners.js
@@ -2,78 +2,81 @@ import React, { Component } from "react";
 import Tabletop from "tabletop";
 
 class Runners extends Component {
-    constructor() {
-        super();
-        this.state = {
-            data: []
-        };
-    }
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
 
-    componentDidMount() {
-        Tabletop.init({
-            key: "1hKeE7XttfUP5KdzgC5QJAB0rcXbzkeq3CBnujbLKpx0",
-            callback: googleData => {
-                this.setState({
-                    runners: googleData.Bieg.elements,
-                    walkers: googleData.NordicWalking.elements
-                });
-                this.renderTableHeader();
-                this.renderTableData();
-            },
-            simpleSheet: false
+  componentDidMount() {
+    Tabletop.init({
+      key: "1hKeE7XttfUP5KdzgC5QJAB0rcXbzkeq3CBnujbLKpx0",
+      callback: googleData => {
+        this.setState({
+          runners: googleData.Bieg.elements,
+          walkers: googleData.NordicWalking.elements
         });
-    }
-    renderTableHeader(collectionName) {
-        if (
-            this.state[collectionName] == null ||
-            this.state[collectionName].length === 0
-        )
-            return;
-        let header = Object.keys(this.state.runners[0]);
-        return header.map((key, index) => {
-            return <th key={index}>{key.toUpperCase()}</th>;
-        });
-    }
+        this.renderTableHeader();
+        this.renderTableData();
+      },
+      simpleSheet: false
+    });
+  }
+  renderTableHeader(collectionName) {
+    if (
+      this.state[collectionName] == null ||
+      this.state[collectionName].length === 0
+    )
+      return;
+    let header = Object.keys(this.state[collectionName][0]);
+    return header.map((key, index) => {
+      return <th key={index}>{key.toUpperCase()}</th>;
+    });
+  }
 
-    renderTableData() {
-        if (this.state.runners == null || this.state.runners.length === 0)
-            return;
-        return this.state.runners.map((runner, index) => {
-            const { Numer, Imie, Nazwisko, Miejscowosc, Klub } = runner; //destructuring
-            return (
-                <tr key={Numer}>
-                    <td>{Numer}</td>
-                    <td>{Imie}</td>
-                    <td>{Nazwisko}</td>
-                    <td>{Miejscowosc}</td>
-                    <td>{Klub}</td>
-                </tr>
-            );
-        });
-    }
+  renderTableData(collectionName) {
+    if (
+      this.state[collectionName] == null ||
+      this.state[collectionName].length === 0
+    )
+      return;
+    return this.state[collectionName].map((runner, index) => {
+      const { Numer, Imie, Nazwisko, Miejscowosc, Klub } = runner; //destructuring
+      return (
+        <tr key={Numer}>
+          <td>{Numer}</td>
+          <td>{Imie}</td>
+          <td>{Nazwisko}</td>
+          <td>{Miejscowosc}</td>
+          <td>{Klub}</td>
+        </tr>
+      );
+    });
+  }
 
-    render() {
-        return (
-            <div className="content-container">
-                <div className="content">
-                    <h1>Lista uczestników biegu:</h1>
-                    <table id="runners">
-                        <tbody>
-                            <tr>{this.renderTableHeader()}</tr>
-                            {this.renderTableData()}
-                        </tbody>
-                    </table>
-                    <h1>Lista uczestników nordic walking:</h1>
-                    <table id="walkers">
-                        <tbody>
-                            <tr>{this.renderTableHeader()}</tr>
-                            {this.renderTableData()}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="content-container">
+        <div className="content">
+          <h1>Lista uczestników biegu:</h1>
+          <table id="runners">
+            <tbody>
+              <tr>{this.renderTableHeader("runners")}</tr>
+              {this.renderTableData("runners")}
+            </tbody>
+          </table>
+          <h1>Lista uczestników nordic walking:</h1>
+          <table id="walkers">
+            <tbody>
+              <tr>{this.renderTableHeader("walkers")}</tr>
+              {this.renderTableData("walkers")}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 Runners.propTypes = {};
