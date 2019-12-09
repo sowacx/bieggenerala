fd350f0ad7246cd23253425584028c3f1e1aa735
Michał Witczak <mich.witczak@gmail.com>
2019-12-10 00:05:46 +0100

    WIP: react-table

--- bieg-generala-app/src/Runners.js
+++ bieg-generala-app/src/Runners.js
@@ -1,5 +1,70 @@
 import React, { Component } from "react";
 import Tabletop from "tabletop";
+import { useTable, useSortBy } from "react-table";
+
+function Table({ columns, data }) {
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useSortBy
+  );
+
+  // We don't want to render all 2000 rows for this example, so cap
+  // it at 20 for this use case
+  const firstPageRows = rows.slice(0, 20);
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                // Add the sorting props to control sorting. For this example
+                // we can add them into the header props
+                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.render("Header")}
+                  {/* Add a sort direction indicator */}
+                  <span>
+                    {column.isSorted
+                      ? column.isSortedDesc
+                        ? " 🔽"
+                        : " 🔼"
+                      : ""}
+                  </span>
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <br />
+      <div>Showing the first 20 results of {rows.length} rows</div>
+    </>
+  );
+}
 
 class Runners extends Component {
   constructor() {
@@ -55,6 +120,17 @@ class Runners extends Component {
     });
   }
 
+  columns = [
+    {
+      Header: "First Name",
+      accessor: "firstName"
+    },
+    {
+      Header: "Last Name",
+      accessor: "lastName"
+    }
+  ];
+
   render() {
     return (
       <div className="content-container">
@@ -73,6 +149,7 @@ class Runners extends Component {
               {this.renderTableData("walkers")}
             </tbody>
           </table>
+          <Table columns={this.columns} data={this.data} />
         </div>
       </div>
     );
