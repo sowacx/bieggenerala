fbadf5e0edd3a21bcef53b3b8ceb24f62bad8d9e
Michał Witczak <mich.witczak@gmail.com>
2019-12-12 00:24:13 +0100

    small fixes

--- bieg-generala-app/src/Nav.js
+++ bieg-generala-app/src/Nav.js
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import logo from "./img/herb.png";
 import { Navbar, Nav } from "react-bootstrap";
 
 class Navigation extends Component {
-    render() {
-        return (
-            <Navbar fixed="top" expand="sm" bg="dark" variant="dark">
-                <Navbar.Brand href="/" className="nav-logo">
-                    <img src={logo} alt="Herb powstania"></img>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="/">O biegu</Nav.Link>
-                        <Nav.Link href="/Runners">Uczestnicy</Nav.Link>
-                        <Nav.Link href="/Rules">Regulamin</Nav.Link>
-                        {/* <Nav.Link href="/Results">Wyniki</Nav.Link> */}
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        );
-    }
+  render() {
+    return (
+      <Navbar fixed="top" expand="sm" bg="dark" variant="dark">
+        <Navbar.Brand href="/" className="nav-logo">
+          <img src={logo} alt="Herb powstania"></img>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">O biegu</Nav.Link>
+            <Nav.Link href="/Runners">Uczestnicy</Nav.Link>
+            <Nav.Link href="/Rules">Regulamin</Nav.Link>
+            {/* <Nav.Link href="/Results">Wyniki</Nav.Link> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 export default Navigation;
