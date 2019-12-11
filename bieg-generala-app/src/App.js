f4afb72895307ace61d03d8ab1f7911e1099b3eb
Michał Witczak <michal.witczak@fi.abb.com>
2019-12-11 14:51:49 +0100

    fix routing

--- bieg-generala-app/src/App.js
+++ bieg-generala-app/src/App.js
@@ -5,18 +5,28 @@ import Runners from "./Runners";
 import Results from "./Results";
 import Rules from "./Rules";
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
     render() {
         return (
-            <div className="body">
-                <Navigation />
-                <Route path="/" exact render={props => <Home />} />
-                <Route path="/Runners" exact render={props => <Runners />} />
-                <Route path="/Results" exact render={props => <Results />} />
-                <Route path="/Rules" exact render={props => <Rules />} />
-            </div>
+            <Router>
+                <div className="body">
+                    <Navigation />
+                    <Route path="/" exact render={props => <Home />} />
+                    <Route
+                        path="/Runners"
+                        exact
+                        render={props => <Runners />}
+                    />
+                    <Route
+                        path="/Results"
+                        exact
+                        render={props => <Results />}
+                    />
+                    <Route path="/Rules" exact render={props => <Rules />} />
+                </div>
+            </Router>
         );
     }
 }
