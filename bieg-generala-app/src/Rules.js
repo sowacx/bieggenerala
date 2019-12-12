577b4523f6bef97f034179c123a503f49cf40a52
Michał Witczak <mich.witczak@gmail.com>
2019-12-12 23:51:13 +0100

    add rules page

--- bieg-generala-app/src/Rules.js
+++ bieg-generala-app/src/Rules.js
@@ -1,15 +1,136 @@
 import React, { Component } from "react";
 
 class Rules extends Component {
-    render() {
-        return (
-            <div className="content-container">
-                <div className="content content-rules">
-                    Regulamin zostanie opublikowany wkrótce
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="content-container">
+        <div className="content content-rules">
+          <div className="content-rules-header">
+            <h2>Regulamin</h2>
+            <h3>Biegu generała Józefa Dowbora Muśnickiego</h3>
+            <h3>28/29 grudnia 2019 Lusowo, 62-080 Tarnowo Podgórne</h3>
+          </div>
+          <hr></hr>
+          <h3>1. ORGANIZATOR</h3>
+          <p>
+            Sołtys i rada sołecka wsi Lusowo, OSP Lusowo. Patronat Towarzystwa
+            Pamięci Gen. Józefa Dowbora Muśnickiego i Muzeum Powstańców
+            Wielkopolskich. im. Gen. Józefa Dowbora Muśnickiego w Lusowie.
+            Kierownik biegu Jolanta Witczak
+          </p>
+          <h3>2. CEL</h3>
+          <p>
+            Upamiętnienie 101 rocznicy Powstania Wielkopolskiego oraz uczczenia
+            Generała Józefa Dowbora-Muśnickiego <br />
+            Popularyzacja biegania jako najprostszej formy aktywności fizycznej.{" "}
+            <br />
+            Promocja sołectwa Lusowa i gminy Tarnowo Podgórne. <br />
+          </p>
+          <h3>3. TERMIN I MIEJSCE </h3>
+          <p>
+            Bazą, startem i metą biegu jest remiza OSP przy ulicy Ogrodowej 3 w
+            Lusowie. Bieg odbędzie się z 28 na 29 grudnia .2019 r. Biuro biegu
+            czynne będzie od godz. 16.30 z weryfikacją zawodników i wydawaniem
+            numerów startowych. <br />
+            -18.30 odprawa techniczna
+            <br />
+            -18.45 przejście uczestników na cmentarz, zapalenie znicza i
+            złożenie wiązanki na grobie gen.Józefa Muśnickiego <br />
+            -19.00 start <br />
+            -11.00 29 grudnia zakończenie zawodów (limit czasowy 16 godzin).{" "}
+            <br /> Organizator w razie trudnych warunków pogodowych może podjąć
+            decyzję o wydłużeniu limitu. <br />
+            Przed rozpoczęciem i po zakończeniu biegu możliwość bezpłatnego
+            zwiedzania Muzeum Powstańców Wielkopolskich.
+            <br />
+          </p>
+          <h3>4.TRASA I DYSTANSE</h3>
+          <p>
+            Trasa nie posiada atestu PZLA.
+            <br />
+            Całkowity dystans biegu to 101 kilometrów, na które składa się 10
+            okrążeń. Trasa wiedzie przez ścieżki o naturalnym podłożu terenu
+            wśród okolicznych łąk i lasów.
+            <br />
+            Zawodnik ma możliwość przebiegnięcia dowolnej liczby okrążeń,
+            zaliczane są tylko pełne okrążenia. <br />
+            Przy każdym zakończonym okrążeniu uczestnik musi zameldować się w
+            biurze biegu. Zawodnik ma prawo do robienia przerw w trakcie zawodów
+            (zejść z trasy) jedynie po pełnych pętlach.
+            <br />
+            Ze względów bezpieczeństwa zawodnicy kończący udział przed
+            wyznaczonym limitem czasowym, proszeni są o przekazanie takiej
+            informacji w biurze zawodów. <br />
+            W bazie zawodów będzie możliwość pozostawienia rzeczy (depozyt),
+            który zostanie zwrócony po okazaniu numeru startowego.
+            <br />W bazie zawodów, dla zawodników będzie bufet, napoje,
+            jedzenie. Po biegu każdy zawodnik może liczyć na ciepły i pożywny
+            posiłek.
+          </p>
+          <h3>5. ZASADY UCZESTNICTWA I ZGŁOSZENIA</h3>
+          <p>
+            Ustalony został limit 80 uczestników. <br />
+            Każdy uczestnik biegu otrzymał indywidualne zaproszenie. Jest to
+            pierwsza edycja biegu, pora zimowa, trasa wymagająca, dlatego w
+            pierwszej edycji organizator chciał uniknąć problemów związanych z
+            logistyką.
+            <br />
+            Organizator nie przewiduje dopuszczenia do biegu osób poniżej 18
+            roku życia.
+            <br />
+            Bieg odbywa się bez profesjonalnego pomiaru czasu ale na czas
+            Maratonu zostanie włączony czas zegarowy wg którego będą podawane
+            czasy końcowe.
+            <br />
+            Każdy uczestnik musi być wyposażony czołówkę/latarkę oświetlającą
+            trasę oraz lampkę z tyłu ciała z czerwonym światłem. <br />
+            Podczas biegu zawodnicy muszą posiadać przy sobie włączony telefon z
+            wpisanym numerem medycznej obsługi biegu 604 444 690 -
+            zabezpieczenie medyczne.
+          </p>
+          <h3>6. KLASYFIKACJA KOŃCOWA I NAGRODY</h3>
+          <p>
+            Przedstawiona zostanie jedna klasyfikacja – kategoria Open. Pomimo
+            prowadzenia klasyfikacji nie przewiduje się nagród dla najlepszych
+            zawodników. <br />
+            Każdy uczestnik biegu otrzyma pamiątkowy medal.
+          </p>
+          <h3>7. OPŁATY</h3>
+          <p>Nie ma opłaty startowej.</p>
+          <h3>8. POSTANOWIENIA KOŃCOWE</h3>
+          <p>
+            Bieg ma charakter koleżeński i towarzyski.
+            <br />
+            Bieg odbędzie się bez względu na pogodę.
+            <br />
+            Uczestnicy mają obowiązek zachowania zasad fair play podczas trwania
+            biegu i na trasie biegu.
+            <br />
+            Uczestnicy mają obowiązek dbać o czystość na trasie i w miejscu
+            mety. <br />
+            Uczestnicy mają obowiązek przestrzegania wytyczonych tras biegowych
+            i nie zbaczać z nich aż do osiągnięcia linii mety. <br />
+            Bieg nie jest ubezpieczony.
+            <br />
+            Organizator zastrzega sobie prawo zakończenia przyjmowania zgłoszeń
+            w momencie przekroczenia liczby 80 uczestników. <br />
+            Zawodnicy startują na własną odpowiedzialność.
+            <br />
+            Organizator nie będzie odpowiadać za przedmioty pozostawione bez
+            zabezpieczenia lub skradzione podczas imprezy.
+            <br />
+            Organizator zastrzega sobie prawo interpretacji niniejszego
+            regulaminu.
+            <br />
+            Niniejszy regulamin zostanie wywieszony przed biegiem i każdy
+            zawodnik będzie musiał podpisać, że zapoznał się z jego treścią
+            podczas odbioru numeru startowego.
+          </p>
+          <p>Więcej informacji udziela Jolanta Witczak- tel.501 946 594</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Rules;
